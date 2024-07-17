97e3ecbc67d4f9d002d929e6e73eec5372c3515a
Fuji <odc@iMac-de-odc-2.local>
2024-07-17 09:44:29 +0100

    emacs settings update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,26 +1,13 @@
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(set-frame-font "SF Mono 10" nil t)
 (load-theme 'whiteboard)
-(setenv "JAVA_HOME"  "/home/fuji/.jdks/corretto-21.0.2")
-(global-font-lock-mode 0) ;; disable colors
-(setq lsp-java-java-path "/home/fuji/.jdks/corretto-21.0.2/bin/java")
-(setq lsp-clients-angular-language-server-command
-  '("node"
-    "/usr/local/lib/node_modules/@angular/language-server"
-    "--ngProbeLocations"
-    "/usr/local/lib/node_modules"
-    "--tsProbeLocations"
-    "/usr/local/lib/node_modules"
-    "--stdio"))
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-  backup-by-copying t    ; Don't delink hardlinks
-  version-control t      ; Use version numbers on backups
-  delete-old-versions t  ; Automatically delete excess backups
-  kept-new-versions 20   ; how many of the newest versions to keep
-  kept-old-versions 5    ; and how many of the old
-)
+  backup-by-copying t
+  version-control t
+  delete-old-versions t
+  kept-new-versions 20
+  kept-old-versions 5)
 
 (setq inhibit-startup-screen t)
 (setq custom-file "~/.emacs.d/custom.el")
@@ -28,8 +15,7 @@
 (setq package-selected-packages '(use-package))
 (setq package-archives '(("elpa" . "https://tromey.com/elpa/")
 			 ("melpa" . "https://melpa.org/packages/")
-			 ("gnu" . "https://elpa.gnu.org/packages/")
-			))
+			 ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -54,6 +40,12 @@
   :config
   (eros-mode 1))
 
+(use-package emacs
+  :custom
+  (tab-always-indent 'complete)
+  (text-mode-ispell-word-completion nil)
+  (read-extended-command-predicate #'command-completion-default-include-p))
+
 (use-package key-chord
   :init
   (setq key-chord-two-keys-default 0.1)
@@ -61,10 +53,36 @@
   :config
   (key-chord-mode 1))
 
-(use-package almost-mono-themes
+(use-package marginalia
   :config
-   (load-theme 'almost-mono-black t)
-  ;; (load-theme 'almost-mono-gray t)
-  ;; (load-theme 'almost-mono-cream t)
-  ;;(load-theme 'almost-mono-white t)
-  )
+  (marginalia-mode 0))
+
+
+(use-package fuzzy)
+(use-package auto-complete
+  :after fuzzy
+  :config
+  (ac-flyspell-workaround)
+  (setq ac-use-fuzzy 1)
+  :init
+  (ac-config-default)
+  (add-to-list 'ac-sources 'ac-source-yasnippet)
+  (global-auto-complete-mode t))
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package lsp-mode)
+(use-package lsp-ui
+  :after lsp-mode
+  :config
+  (setq lsp-ui-doc-enable t)
+  (setq lsp-ui-sideline-enable t)
+  (setq lsp-ui-doc-position 'at-point)
+  (setq lsp-ui-sideline-show-hover nil)
+  (setq lsp-ui-sideline-show-diagnostics t)
+  (setq lsp-ui-sideline-delay 0.1)
+  (setq lsp-ui-doc-show-with-cursor nil))
